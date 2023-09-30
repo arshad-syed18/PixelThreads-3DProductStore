fff56a773bf8e70537513a3a5afb3bff9fd92dcb
DarkkLegion <arshad18syed@gmail.com>
2023-09-30 20:14:51 +0530

    bottom tabs animated

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -41,6 +41,17 @@ const Customizer = () => {
                             customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                         />
                     </motion.div>
+                    <motion.div className='filtertabs-container' {...slideAnimation("up")}>
+                        {FilterTabs.map((tab, index) => (
+                            <Tab
+                                key={tab.name}
+                                tab={tab}
+                                isFilterTab
+                                isActiveTab=""
+                                handleCLick={() => { }}
+                            />
+                        ))}
+                    </motion.div>
                 </>
             )}
         </AnimatePresence>
