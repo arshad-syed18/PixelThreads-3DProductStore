e4892a231957f6d52ad25a91020721722400a962
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 11:42:16 +0530

    3D model responsive design sizes defined

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -12,7 +12,18 @@ const CameraRig = ({ children }) => {
         const isBreakpoint = window.innerWidth <= 1260;
         const isMobile = window.innerWidth <= 600;
 
-        //set initial position 
+        //set initial position
+        let targetPosition = [-0.4, 0, 2];
+        if (snap.intro) {
+            if (isBreakpoint) targetPosition = [0, 0, 2];
+            if (isMobile) targetPosition = [0, 0.2, 2.5];
+        } else {
+            if (isMobile) targetPosition = [0, 0, 2.5];
+            else targetPosition = [0, 0, 2];
+        }
+
+        //set camera position
+        easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
         easing.dampE(
             group.current.rotation,
