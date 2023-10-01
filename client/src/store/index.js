b47356a86ffec97e3b441a486321e1b468fb0726
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 13:44:25 +0530

    state value changes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ const state = proxy({
     intro: true,
     color: '#EFBD4E',
     isLogoTexture: true,
-    isFullTexture: true,
+    isFullTexture: false,
     logoDecal: './threejs.png',
     fullDecal: './threejs.png',
 });
