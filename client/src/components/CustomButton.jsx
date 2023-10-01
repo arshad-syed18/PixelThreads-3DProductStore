10bd7ffeeedeeef2c9bb56499600f85b6d9724cb
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 17:01:33 +0530

    outline styles, contrasting text color

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSnapshot } from 'valtio'
 import state from '../store'
+import { getContrastingColor } from '../config/helpers'
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
     const snap = useSnapshot(state)
@@ -8,8 +9,13 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
         if (type === 'filled') {
             return {
                 backgroundColor: snap.color,
-                color: "#fff",
-                textShadow: '0px 0px 2px #000',
+                color: getContrastingColor(snap.color),
+            }
+        } else if (type === 'outline') {
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color,
             }
         }
     }
