b1e566301e8e174616b346c4b278dcfb3b2a7a4d
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 13:45:08 +0530

    backdrop shadow with lights added

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
+    const shadows = useRef();
     return (
-        <div>Backdrop</div>
+        <AccumulativeShadows
+            ref={shadows}
+            temporal
+            frames={60}
+            alphaTest={0.85}
+            scale={4}
+            rotation={[Math.PI / 2, 0, 0]}
+            position={[0, 0, -0.14]}
+        >
+            <RandomizedLight
+                amount={4}
+                radius={10}
+                intensity={1.4}
+                ambient={0.25}
+                position={[5, 5, -9]}
+            />
+            <RandomizedLight
+                amount={4}
+                radius={12}
+                intensity={1.4}
+                ambient={0.55}
+                position={[-5, 5, -9]}
+            />
+        </AccumulativeShadows>
     )
 }
 
