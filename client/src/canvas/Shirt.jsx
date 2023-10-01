69cf12abde272bf7bda783164bffd5c81d978781
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 11:53:38 +0530

    color transition and render bug fix

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -11,8 +11,14 @@ const Shirt = () => {
 
     const logoTexture = useTexture(snap.logoDecal);
     const fullTexture = useTexture(snap.fullDecal);
+    //used to apply color smoothly, transition from one color to another
+    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+    const stateString = JSON.stringify(snap);
     return (
-        <group>
+        <group
+            key={stateString}
+        >
             <mesh
                 castShadow
                 geometry={nodes.T_Shirt_male.geometry}
@@ -20,7 +26,25 @@ const Shirt = () => {
                 material-roughness={1}
                 dispose={null}
             >
-
+                {snap.isFullTexture && (
+                    <Decal
+                        position={[0, 0, 0]}
+                        rotation={[0, 0, 0]}
+                        scale={1}
+                        map={fullTexture}
+                    />
+                )}
+                {snap.isLogoTexture && (
+                    <Decal
+                        position={[0, 0.04, 0.15]}
+                        rotation={[0, 0, 0]}
+                        scale={0.15}
+                        map={logoTexture}
+                        map-anisotropy={16}
+                        depthTest={false}
+                        depthWrite={true}
+                    />
+                )}
             </mesh>
         </group>
     )
