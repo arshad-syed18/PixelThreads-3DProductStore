dab3b1510bce241f5633ccbecb8a22d018b2847d
DarkkLegion <arshad18syed@gmail.com>
2023-10-01 17:01:10 +0530

    Tab styling bug fixes

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -13,14 +13,15 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
     return (
         <div
             key={tab.name}
-            className={`tab-btn ${isFilterTab ? 'rounded-full glassmorhism' : 'rounded-4'}`}
+            className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
             onClick={handleClick}
+            style={activeStyles}
 
         >
             <img
                 src={tab.icon}
                 alt={tab.name}
-                className={`$isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
+                className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain}'}`}
             />
         </div>
     )
