ad099c1530396925773b39fa11d9e3239f69a751
DarkkLegion <arshad18syed@gmail.com>
2023-10-06 21:28:01 +0530

    Dall-e image generation code added

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -9,7 +9,19 @@ const router = express.Router();
 // no credits remaining, change to different model
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
 
+        const image = await openai.images.generate({ prompt: prompt })// wont work no credits
+
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+    console.log(response);
+})
 
 router.route('/').get((req, res) => {
     res.status(200).json({ message: 'Hello World Route!' });
