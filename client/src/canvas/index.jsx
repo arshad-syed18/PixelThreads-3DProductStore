e14f928b70dee8bc7660dde3618ff4f6bcf1a916
DarkkLegion <arshad18syed@gmail.com>
2023-12-09 17:30:40 +0530

    3D Model switching logic

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,28 +2,41 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei'
 
-import Shirt from './Shirt'
+import { Shirt } from './Shirt'
 import Backdrop from './Backdrop'
 import CameraRig from './CameraRig'
 import { Tshirt } from './Tshirt'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
 
 const CanvasModel = () => {
+    const snap = useSnapshot(state)
+
+    const generateModel = () => {
+        switch (snap.model) {
+            case 'tshirt':
+                return <Shirt key="tshirt" />
+            case 'poloShirt':
+                return <Tshirt key="poloShirt" />
+            default:
+                return null;
+        }
+    }
     return (
         <Canvas
             shadows
             camera={{ position: [0, 0, 0], fov: 25 }}
             gl={{ preserveDrawingBuffer: true }}
             className='w-full max-w-full h-full transition-all ease-in'
+            position={[0, 0, 0]}
         >
             <ambientLight intensity={0.5} />
             <Environment preset="city" />
 
             <CameraRig>
                 <Backdrop />
-                <Center>
-                    <Tshirt />
-                </Center>
+                {generateModel()}
             </CameraRig>
         </Canvas >
     )
