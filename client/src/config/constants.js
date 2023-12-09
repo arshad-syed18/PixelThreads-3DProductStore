89adddcbc16595b9f6775c0cfbb98da43d5433c5
DarkkLegion <arshad18syed@gmail.com>
2023-12-09 15:03:34 +0530

    added multiple 3d model support

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -4,14 +4,17 @@ export const EditorTabs = [
   {
     name: "colorpicker",
     icon: swatch,
+    helperText: "Color",
   },
   {
     name: "filepicker",
     icon: fileIcon,
+    helperText: "File",
   },
   {
     name: "aipicker",
     icon: ai,
+    helperText: "AI",
   },
 ];
 
@@ -19,10 +22,24 @@ export const FilterTabs = [
   {
     name: "logoShirt",
     icon: logoShirt,
+    helperText: "Logo",
   },
   {
     name: "stylishShirt",
     icon: stylishShirt,
+    helperText: "Texture",
+  },
+];
+export const modelTabs = [ // Limit helpertext length to 6-7 characters
+  {
+    name: "tshirt",
+    icon: logoShirt, // TODO: change to tshirt icon
+    helperText: "T-Shirt",
+  },
+  {
+    name: "poloShirt",
+    icon: stylishShirt, // TODO: change to poloShirt icon
+    helperText: "Polo",
   },
 ];
 
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,6 +7,7 @@ const state = proxy({
     isFullTexture: false,
     logoDecal: './threejs.png',
     fullDecal: './threejs.png',
+    model: 'tshirt'
 });
 
 export default state;
