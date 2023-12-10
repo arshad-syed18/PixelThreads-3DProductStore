c851f911419d6c223d91328d1052da62caaa0abd
DarkkLegion <arshad18syed@gmail.com>
2023-12-10 20:14:17 +0530

    Redesigned landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import {
 } from '../config/motion'
 import state from '../store'
 import { CustomButton } from '../components'
+import { logo } from '../assets'
 
 const Home = () => {
     const snap = useSnapshot(state)
@@ -17,7 +18,7 @@ const Home = () => {
             {snap.intro && (
                 <motion.section className='home' {...slideAnimation('left')}>
                     <motion.header className='flex' {...slideAnimation('down')}>
-                        <img src='./threejs.png' alt='threejs' className='w-8 h-8 object-contain mr-4' />
+                        <img src={logo} alt='logo' className='w-8 h-8 object-contain mr-4' />
                         <div className='flex flex-col gap-1 mt-[-5px]'>
                             <h1 className='font-bold tracking-wider text-4xl text-gray-800 '>PixelThreads</h1>
                         </div>
@@ -25,21 +26,21 @@ const Home = () => {
                     <motion.div className='home-content' {...headContainerAnimation}>
                         <motion.div {...headTextAnimation}>
                             <h1 className='head-text'>
-                                LET'S <br className='xl:block hidden' /> DO IT.
+                                CREATE <br className='xl:block hidden' /> MAGIC.
                             </h1>
                         </motion.div>
                         <motion.div className='flex flex-col gap-5' {...headContentAnimation}>
                             <p className='max-w-md font-normal text-gray-600 text-base'>
-                                Create your unique and exclusive shirt with our brand new
-                                3D Customization tool. <strong>Unleash your imagination</strong>{" "}
-                                and define your style.
+                                <strong className='text-gray-700 text-center'>Design. Express. Impress. </strong>{" "}
+                                Discover endless possibilities with our new 3D Customization tool.
+                                Unleash your imagination and tailor your fashion to reflect your individuality."
                             </p>
 
                             <CustomButton
                                 type="filled"
                                 title="Customize"
                                 handleClick={() => state.intro = false}
-                                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                                customStyles="w-fit px-4 py-2.5 font-bold text-sm "
                             />
                         </motion.div>
                     </motion.div>
