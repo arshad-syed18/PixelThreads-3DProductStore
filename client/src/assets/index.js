a268eecebfc60802e937b8fd732a080f6e7ff7be
DarkkLegion <arshad18syed@gmail.com>
2023-12-10 20:13:46 +0530

    New logo

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -5,5 +5,6 @@ import download from "./download.png";
 
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
+import logo from "./logo.png";
 
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
+export { ai, fileIcon, swatch, download, logoShirt, stylishShirt, logo };
