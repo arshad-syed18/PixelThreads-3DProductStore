d348d2f7e9f730e73e472fe36b8a6cc8c71e3e13
DarkkLegion <arshad18syed@gmail.com>
2023-12-15 17:16:05 +0530

    deployment port added

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import sdRoutes from './routes/stablediffusion.routes.js';
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
@@ -16,6 +17,6 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('Server listening on port 8080');
 });
